6b8f07e3895012e882c806ab522220b4923f8630
Patrick Lazarus <plazarus@mpifr-bonn.mpg.de>
2012-03-06 23:06:54 +0100

    Added (single comp.) gaussian red-chi2 rating.

---
+++ raters/gaussian_goodness.py
@@ -0,0 +1,31 @@
+import base
+from rating_classes import gaussian
+
+class GaussianGofRater(base.BaseRater):
+    short_name = "gaussgoodness"
+    long_name = "Gaussian Goodness Rating"
+    description = "The reduced chi2 of a single-Gaussian fit to the best " \
+                  "profile. Good fits should have a reduced chi2 of ~1"
+    version = 1
+
+    rat_cls = gaussian.SingleGaussianProfileClass()
+
+    def _compute_rating(self, cand):
+        """Return a rating for the candidate. The rating value is the
+            reduced chi2 of the gaussian function fit to the candidate's 
+            profile.
+        
+            Input:
+                cand: A Candidate object to rate.
+
+            Output:
+                value: The rating value.
+        """
+        profile = cand.profile
+        sgauss = cand.singlegaussfit
+        chi2 = sgauss.get_chisqr()
+        dof = sgauss.get_dof(len(profile))
+        return chi2/dof
+
+
+Rater = GaussianGofRater
