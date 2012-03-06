610d8bee49a6e32480682ccd938ff1097df4f83d
Patrick Lazarus <plazarus@mpifr-bonn.mpg.de>
2012-03-06 23:05:01 +0100

    Added (single comp.) gaussian amplitude rating.

---
+++ raters/gaussian_amplitude.py
@@ -0,0 +1,37 @@
+import base
+from rating_classes import gaussian
+
+class GaussianAmplitudeRater(base.BaseRater):
+    short_name = "gaussamplitude"
+    long_name = "Gaussian Amplitude Rating"
+    description = "The amplitude of a single-Gaussian fit to the best " \
+                  "profile, normalized such that the profile standard " \
+                  "deviation is 1."
+    version = 1
+
+    rat_cls = gaussian.SingleGaussianProfileClass()
+
+    def _compute_rating(self, cand):
+        """Return a rating for the candidate. The rating value is the
+            amplitude of the gaussian function fit to the candidate's 
+            profile.
+        
+            Input:
+                cand: A Candidate object to rate.
+
+            Output:
+                value: The rating value.
+        """
+        sgauss = cand.singlegaussfit
+        ncomp = len(sgauss.components)
+        if ncomp == 1:
+            amp = sgauss.components[0].amp
+        elif ncomp  == 0:
+            amp = 0.0
+        else:
+            raise utils.RatingError("Bad number of components for single " \
+                                    "gaussian fit (%d)" % ncomp)
+        return amp
+
+
+Rater = GaussianAmplitudeRater
