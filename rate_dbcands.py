15ed69ba9ba6002f9f402d25e4210bf9c0cc7f68
Patrick Lazarus <plazar@physics.mcgill.ca>
2012-04-15 05:48:25 -0400

    Major work on 'rate_dbcands.py'.

--- rate_dbcands.py
+++ rate_dbcands.py
@@ -5,10 +5,17 @@ import copy
 import multiprocessing
 import traceback
 import textwrap
+import tempfile
+import shutil
+import subprocess
+import os
+import os.path
 
 import candidate
 import raters
+import database
 
+DBNAME = "common-copy"
 
 def rate_pfd(pfdfn, rater_instances):
     """Given the name of a *.pfd file and a list of Rater instances
@@ -30,7 +37,91 @@ def rate_pfd(pfdfn, rater_instances):
     return cand
 
 
+def download_many(ftpfns, outdir):
+    """Download multiple files from the cornell FTP server to
+        the specified output directory.
+
+        Input:
+            ftpfns: A list of files on the FTP server to get.
+            outdir: The directory to put downloaded files in.
+
+        Outputs:
+            None
+    """ 
+    lftp_cmd = '"get -e -O %s %s"' % (outdir, " ".join(ftpfns))
+    cmd = "lftp -c 'open -e %s -u %s,%s -p 31001 arecibo.tc.cornell.edu'" % \
+             (lftp_cmd, config.ftp_username, config.ftp_password)
+    subprocess.call(cmd, stdout=open(os.devnull), shell=True)
+
+
+def get_beams_to_rate(rat_inst_id):
+    """Given a pdm_rating_instance_id value from the database
+        return a list of header_id values that have candidates
+        that need to be rated by this rating instance.
+
+        Input:
+            rat_inst_id: Rating instance ID value.
+        
+        Output:
+            header_ids: Header ID values for beams that have candidates
+                to be rated by this rating instance.
+    """
+    db = database.Database(DBNAME)
+
+    # get header IDs of candidates that are missing this rating
+    db.execute("SELECT h.header_id " \
+               "FROM pdm_candidates AS c WITH(NOLOCK) " \
+               "LEFT JOIN headers AS h WITH(NOLOCK) " \
+                   "ON c.header_id=h.header_id " \
+               "LEFT JOIN pdm_rating AS r WITH(NOLOCK) " \
+                   "ON c.pdm_cand_id=r.pdm_cand_id " \
+                       "AND r.pdm_rating_instance_id=? " \
+               "WHERE r.pdm_rating_instance_id IS NULL " \
+               "GROUP BY h.header_id", rat_inst_id)
+    rows = db.fetchall()
+    header_ids = [row[0] for row in rows]
+    return header_ids
+   
+
+def get_pfds(header_id):
+    """Given a header ID value download the associated candidates'
+        pfds to a temporary directory. Return the path to the temp dir.
+
+        Input:
+            header_id: The header ID value to download pfds for.
+
+        Output:
+            tempdir: The temporary directory containing the pfds.
+            fn_mapping: A dictionary mapping pdm_cand_id to pfd filename.
+    """
+    db = database.Database()
+    # Get paths/filenames for uploaded pfds
+    query = "SELECT pdm_cand_id, file_location, filename " \
+            "FROM PDM_Candidate_Binaries_Filesystem AS cbf WITH(NOLOCK) " \
+            "LEFT JOIN pdm_candidates AS c WITH(NOLOCK) " \
+                "ON c.pdm_cand_id=cbf.pdm_cand_id " \
+            "WHERE c.header_id=?"
+    db.execute(query, header_id)
+    rows = db.fetchall()
+    fn_mapping = {}
+    pfds_to_get = []
+    for row in rows:
+        fn_mapping[row[0]] = row[2]
+        pfds_to_get.append(os.path.join(row[1], row[2]))
+    
+    # Create temporary directory
+    tempdir = tempfile.mkdtemp(suffix='ratings2')
+
+    # Download files
+    download_many(pfds_to_get, tempdir)
+
+    return tempdir, fn_mapping
+
+
 def main():
+    if args.num_procs > 1:
+        warning.warn("Multithreading not implemnted (%d threads requested)" % \
+                            args.num_procs)
     if args.list_raters:
         print "Number of raters registered: %d" % len(raters.registered_raters)
         for rater_name in raters.registered_raters:
@@ -44,45 +135,63 @@ def main():
                 print "-"*25
         sys.exit(0)
 
-    rater_instances = []
+    rat_inst_id_cache = utils.RatingInstanceIDCache(DBNAME)
+    loaded_raters = {}
     for rater_name in args.raters:
         rater_module = getattr(raters, rater_name)
-        rater_instances.append(rater_module.Rater())
+        rater = rater_module.Rater()
+        loaded_raters[(rater.long_name, rater.version)] = rater
+        
+    for rater in loaded_raters.values():
+        rating_instance_id = rat_inst_id_cache.get_id(rater.long_name, \
+                                                      rater.description)
+        header_ids = get_beams_to_rate(rating_instance_id)
+        for header_id in header_ids:
+            # For candidates with this header_id find which current ratings 
+            # are not computed
+            query = "SELECT c.pdm_cand_id, " \
+                        "rt.name, " \
+                        "ri.version, " \
+                    "FROM pdm_candidates AS c WITH(NOLOCK) " \
+                    "CROSS JOIN (SELECT rt.pdm_rating_type_id, " \
+                                    "MAX(ri.pdm_rating_instance_id) " \
+                                        "AS current_instance_id " \
+                                "FROM pdm_rating_instance AS ri WITH(NOLOCK) " \
+                                "LEFT JOIN pdm_rating_type AS rt WITH(NOLOCK) " \
+                                    "ON ri.pdm_rating_type_id=rt.pdm_rating_type_id " \
+                                "GROUP BY rt.pdm_rating_type_id) AS ci " \
+                    "LEFT JOIN pdm_rating_instance AS ri WITH(NOLOCK) " \
+                        "ON ri.pdm_rating_instance_id=ci.current_instance_id " \
+                    "LEFT JOIN pdm_rating AS r WITH(NOLOCK) " \
+                        "ON r.pdm_cand_id=c.pdm_cand_id " \
+                            "AND ri.pdm_rating_instance_id=r.pdm_rating_instance_id " \
+                    "LEFT JOIN pdm_rating_type AS rt WITH(NOLOCK) " \
+                        "ON rt.pdm_rating_type_id=ri.pdm_rating_type_id " \
+                    "WHERE c.header_id=? AND r.value IS NULL"
+            db.execute(query, header_id)
+            missing_ratings = db.fetchall()
 
-    cands = []
-    if args.num_procs > 1:
-        print "Using %d rater threads" % args.num_procs
-        rater_pool = multiprocessing.Pool(processes=args.num_procs)
-        apply_async = lambda pfdfn: rater_pool.apply_async(rate_pfd, \
-                                                (pfdfn, rater_instances))
-        inprogress = [apply_async(pfdfn) for pfdfn in args.infiles]
-        failed = []
-        while inprogress:
-            for ii in range(len(inprogress))[::-1]:
-                result = inprogress[ii]
-                if result.ready():
-                    if result.successful():
-                        cands.append(result.get())
-                    else:
-                        failed.append(result)
-                    inprogress.pop(ii)
-        print "Number of failures detected: %d" % len(failed)
-        for fail in failed:
-            try:
-                print type(fail), fail
-                fail.get()
-            except:
-                traceback.print_exc()
-    else:
-        for pfdfn in args.infiles:
-            cands.append(rate_pfd(pfdfn, rater_instances))
-    for cand in cands:
-        if args.write_to_file:
-            cand.write_ratings_to_file()
-        if args.write_to_screen:
-            print cand.pfdfn
-            print cand.get_ratings_overview()
-            print '-'*25
+            if not missing_ratings:
+                raise utils.RatingError("At least the current rating should " \
+                                    "be missing. (This is how the header "
+                                    "IDs were selected.)")
+
+            # Get pfds for this header_id
+            tmpdir, fn_mapping = get_pfds(header_id)
+
+            # Rate pfds for this header_id
+            for cand_id, pfd_fn in fn_mapping.iteritems():
+                raters_to_use = [loaded_raters[(x[1], x[2])] for x in missing_ratings \
+                                    if x[0]==cand_id]
+                rated_cands.append(rate_pfd(os.path.join(tmpdir, pfd_fn), raters_to_use))
+
+            # Upload rating values
+            for cand in rated_cands:
+                print cand.get_ratings_overview()
+                
+            # Remove the temporary directory containing pfd files
+            shutil.rmtree(tmpdir)
+
 
 class RemoveAllRatersAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
