e16a92432202b7bd4f460aa2a695ff4431fd1b6c
Patrick Lazarus <plazar@physics.mcgill.ca>
2012-04-17 05:08:29 -0400

    Add/register 2 persistence/broadbandedness ratings.

---
+++ raters/subbands_snrstd.py
@@ -0,0 +1,26 @@
+import base
+from rating_classes import subband_stats
+
+class SubbandsSNRStdev(base.BaseRater):
+    short_name = "subbands_snrstd"
+    long_name = "Subband SNR Std Dev"
+    description = "Determines the standard deviation of the subband " \
+                  "signal-to-noise ratios."
+    version = 2
+    rat_cls = subband_stats.SubbandPulseWindowStats()
+
+    def _compute_rating(self, cand):
+        """Return a rating for the candidate. The rating value is the
+            standard deviation of the sub-band SNRs.
+
+            Input:
+                cand: A Candidate object to rate.
+
+            Output:
+                value: The rating value.
+        """
+        chanstats = cand.subband_stats
+        return max((chanstats.get_snr_stddev(), chanstats.get_peak_snr_stddev()))
+
+
+Rater = SubbandsSNRStdev 
---
+++ raters/subints_snrstd.py
@@ -0,0 +1,26 @@
+import base
+from rating_classes import subint_stats
+
+class SubintsSNRStdev(base.BaseRater):
+    short_name = "subints_snrstd"
+    long_name = "Subint SNR Std Dev"
+    description = "Determines the standard deviation of the subint " \
+                  "signal-to-noise ratios."
+    version = 2
+    rat_cls = subint_stats.SubintPulseWindowStats()
+
+    def _compute_rating(self, cand):
+        """Return a rating for the candidate. The rating value is the
+            standard deviation of the sub-int SNRs.
+
+            Input:
+                cand: A Candidate object to rate.
+
+            Output:
+                value: The rating value.
+        """
+        intstats = cand.subint_stats
+        return max((intstats.get_snr_stddev(), intstats.get_peak_snr_stddev()))
+
+
+Rater = SubintsSNRStdev 
